be32696e92d00b0133735c800ef6d433c063205d
Oliver Collins <o.collins@arkoselabs.com>
2020-03-02 18:47:51 +1000

    Added challenges 7 & 8

--- 0x01/courseData.sh
+++ 0x01/courseData.sh
@@ -3,6 +3,8 @@ flag2="flag{2}";
 flag3="flag{3}";
 flag4="flag{4}";
 flag5="flag{5}";
+flag7="flag{7}";
+flag8="flag{8}";
 # Challenge 1 - Normal files
 echo $flag1 > ~/welcome
 # Challenge 2 - Hidden files
@@ -18,6 +20,13 @@ crontab -l | { cat; echo "* * * * * echo my_JOB_is_to_hide_flags >> /var/log/sys
 crontab -l | { cat; echo "#" $flag5; } | crontab -
 # Challenge 6 - Hey man checkout my code
 #git clone https://github.com/oliverjcollins/flag
+# Challenge 7 - Let's get scripty
+echo export PS1="""root@teaching-server $ """ >> ~/ch7.sh
+echo $flag7 >> ch7.sh
+# Challenge 8 - There's somebody else here
+useradd oliver -p $flag8
+# Challenge 9 - 
+
 # Hide our challenges from the user
 history -c
 # Make their prompt a bit more special
