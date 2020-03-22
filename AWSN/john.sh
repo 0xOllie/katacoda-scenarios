4bb2ec793d6e10dc92cd87112c7afd942983253d
Oliver Collins <o.collins@arkoselabs.com>
2020-03-22 18:43:11 +1000

    John killed Hashcat

--- AWSN/john.sh
+++ AWSN/john.sh
@@ -1,12 +1,9 @@
 # Create files
-mkdir hashcat
-cd hashcat
+mkdir john
+cd john
 
-# Install dependancies
-sudo apt install p7zip-full
-
-# Download Hashcat
-wget https://hashcat.net/files/hashcat-5.1.0.7z
+# Download & unpack John the Ripper
+sudo snap install john-the-ripper
 
 # Generate Hash
 wordlist=("ashley" "qwerty" "111111" "iloveu" "0" "michelle" "tigger" "sunshine" "chocolate" "password1" "soccer" "anthony" "friends" "butterfly" "purple" "angel" "jordan" "liverpool" "justin" "loveme" "fuckyou" "123123" "football" "secret" "andrea" "carlos" "jennifer" "joshua" "bubbles" "1234567890" "superman" "hannah" "amanda" "loveyou" "pretty" "basketball" "andrew" "angels" "tweety" "flower" "playboy" "hello" "elizabeth" "hottie" "tinkerbell" "charlie" "samantha" "barbie" "chelsea" "lovers" "teamo" "jasmine" "brandon" "666666" "shadow" "melissa" "eminem" "matthew" "robert" "danielle" "forever" "family" "jonathan" "987654321" "computer" "whatever" "dragon" "vanessa" "cookie" "naruto" "summer" "sweety" "spongebob" "joseph" "junior" "softball" "taylor" "yellow" "daniela" "lauren" "mickey" "princesa")
@@ -17,8 +14,6 @@ echo "Hey if you are in this folder you should be using the Learning to Meow wit
 
 # Write a hash to a file
 echo -n ${wordlist[$rand]} | md5sum | tr -d " -" >> hash.txt
-# Write wordlist to file
-printf "%s\n" "${wordlist[@]}" > wordlist.txt
 
 # Hide our challenges from the user
 history -c
