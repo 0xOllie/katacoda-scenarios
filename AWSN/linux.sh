056fadf0080d1d250db91d95f3a1d88cf1dcc550
Oliver Collins <o.collins@arkoselabs.com>
2020-03-22 17:49:23 +1000

    Fixed bugged ch7

--- AWSN/linux.sh
+++ AWSN/linux.sh
@@ -21,9 +21,12 @@ crontab -l | { cat; echo "#" $flag5; } | crontab -
 # Challenge 6 - Hey man checkout my code
 #git clone https://github.com/oliverjcollins/flag
 # Challenge 7 - Let's get scripty
-echo "There seems to be a syntax error in this script, try and solve it" 
-echo export PS1="root@teaching-server $ " >> ~/ch7.sh
-echo $flag7 >> ch7.sh
+echo '#!/bin/bash
+echo export PS1='root@teaching-server $ '
+echo echo Run me ^^^
+# $flag7
+' > ~/ch7.sh
+
 # Challenge 8 - There's somebody else here
 useradd oliver -p $flag8
 # Hide our challenges from the user
